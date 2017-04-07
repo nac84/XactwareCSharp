60a7fe0fa4b40d502e0dd03a12787dc5c6ee6abc
Nate <nacortez@xactware.com>
2017-04-07 14:31:53 -0600

    Ch5 Variables

---
+++ Ch5Variables/Ch 5 Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_5_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            int a = 5;
+            int b = 2;
+            b = a;
+            a = -3;
+            Console.WriteLine(score);
+            Console.WriteLine(b);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
